6f561b0fc25ce0a208d1054d1b843912be4b9433
Martin Björklund <mbj4668@gmail.com>
2015-08-27 10:34:42 +0200

    removed deprecated XSD plugin; won't be updated for 1.1

--- test/selftest.py
+++ test/selftest.py
@@ -11,7 +11,6 @@ from pyang import util
 from pyang import error
 from pyang import grammar
 from pyang import syntax
-import pyang.translators.xsd as xsd
 
 def oscmd(cmd):
     p = subprocess.Popen(cmd, shell=True,
@@ -42,7 +41,6 @@ def chk_error_codes():
 def chk_stmts():
     global found_error
     stmtmaps = [(grammar.stmt_map, "grammar.stmt_map"),
-                (xsd.yang_keywords, "xsd.yang_keywords"),
                 (syntax.yin_map, "syntax.yin_map")]
     for (map, name) in stmtmaps:
         for stmt in map:
@@ -54,17 +52,6 @@ def chk_stmts():
                     found_error = True
     if found_error:
         return
-    for s in syntax.yin_map:
-        (argname, yinelem) = syntax.yin_map[s]
-        (xargname, xyinelem, xsdappinfo) = xsd.yang_keywords[s]
-        if argname != xargname:
-            sys.stderr.write("Stmt %s argname mismatch in syntax.yin_map " \
-                                 "and xsd.yang_keywords" % s)
-            found_error = True
-        if yinelem != xyinelem:
-            sys.stderr.write("Stmt %s yinelem mismatch in syntax.yin_map " \
-                                 "and xsd.yang_keywords" % s)
-            found_error = True
 
 chk_error_codes()
 chk_stmts()
